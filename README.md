016e58150e02ae72889e3e7ea166031ba9797166
Sadanand Pai <sadypai@gmail.com>
2026-05-02 15:10:04 +0530

    'init'

---
+++ README.md
@@ -0,0 +1,5 @@
+# ML Mini Challenges
+
+Welcome to the GitHub repository for ML Mini Challenges.
+
+This is a collection of mini challenges to help you learn and practice machine learning concepts.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mt-4 mb-4 text-center">
+        ML Mini Challenges
+      </h1>
+      <p className="text-center mb-4">
+        Welcome to the official GitHub repository for ML Mini Challenges.
+      </p>
+    </>
+  );
+}
